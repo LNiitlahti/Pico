46b031d05969f6bf955c0157b805451aea096d9a
Lassi Niitlahti <lniitlahti@gmail.com>
2025-09-30 18:04:19 +0300

    Task1 initial commit

---
+++ lesson1/task1_print_numbers.py
@@ -0,0 +1,11 @@
+# task1_print_numbers.py - Print numbers 0-9 (C) Lassi Niitlahti
+
+# This program prints numbers from 0 to 9
+# Simple loop to demonstrate basic Python programming on Pico
+
+# Loop through numbers 0 to 9
+for i in range(10):
+    print(i)
+
+# Print completion message
+print("Done printing numbers!")
